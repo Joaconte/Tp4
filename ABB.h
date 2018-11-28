2fe42ded5b95de8f5a913c880d58f0725e853510
nres98 <43898082+nres98@users.noreply.github.com>
2018-11-28 13:50:22 -0300

    Add files via upload

--- ABB.h
+++ ABB.h
@@ -9,12 +9,32 @@ class Abb{
 
 	public:
 	//metodos
+
+        //constructor sin parametros
+        //pre: -
+        //post: construye el ABB con raiz = 0
         Abb();
+
+        //constructor con parametros
+        //pre: recibe el puntero a la raiz
+        //post: construye el ABB con la raiz recibida
         Abb(Nodo* raiz);
+
+        //pre: -
+        //post: devuelve la raiz
         Nodo* obtenerRaiz();
+
+        //pre: recibe un nuevo aeropuerto y el nodo actual
+        //post: agrega el aeropuerto en la posicion correspondiente
         void agregarElemento(Nodo*aeropuerto, Nodo* actual);
+
+        //pre: recibe el nodo actual
+        //post: recorre el arbol de manera inOrder
         void inOrder(Nodo* actual);
+
+        //destructor
         ~Abb();
+
 
 };
 
--- Nodo.h
+++ Nodo.h
@@ -25,13 +25,37 @@ class Nodo {
 
     //metodos
 	public:
+	    //constructor
+	    //pre: recibe la clave del aeropuerto y un puntero al resto de los datos
+	    //post: construye el nodo
         Nodo(Tipo clave, aeropuerto* datos);
+
+        //pre: -
+        //post: devuelve la clave
         Tipo obtenerClave();
+
+        //pre: -
+        //post: devuelve los datos del aeropuerto
         aeropuerto* obtenerDatos();
+
+        //pre: -
+        //post: devuelve un puntero al nodo izquierdo
         Nodo* obtenerIzquierdo();
+
+        //pre: -
+        //post: devuelve un puntero al nodo derecho
         Nodo* obtenerDerecho();
+
+        //pre: recibe un puntero a nodo
+        // post: asigna ese puntero como izquierdo
         void asignarIzquierdo(Nodo* punteroNodo);
+
+
+        //pre: recibe un puntero a nodo
+        // post: asigna ese puntero como derecho
         void asignarDerecho(Nodo* punteroNodo);
+
+        //destructor
         ~Nodo();
 };
 
